08c50bc8330c6be2c9f13556bf50a4aabe210496
Sergey Bagdasaryan <81790360+sergiobagd@users.noreply.github.com>
2023-03-30 09:52:51 +0500

    chore: update samples quantity

--- get_data.py
+++ get_data.py
@@ -5,7 +5,7 @@ import os
 
 seed = 42
 # Generate data
-X, y = make_classification(n_samples=1000, random_state=seed)
+X, y = make_classification(n_samples=534, random_state=seed)
 
 # Make a train/test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
